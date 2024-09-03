4253e19f0d82dce9952602fcdde174e1f320c714
LuizRossini <luiz.guirossini123@gmail.com>
2024-09-03 19:17:41 -0300

    first commit

---
+++ src/main/java/exercises/beecrowd/BeecrowdApplication.java
@@ -0,0 +1,10 @@
+package exercises.beecrowd;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeecrowdApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BeecrowdApplication.class, args);
+	}
+}
---
+++ src/test/java/exercises/beecrowd/BeecrowdApplicationTests.java
@@ -0,0 +1,13 @@
+package exercises.beecrowd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeecrowdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
