90b91e17027eb7c9f4fea55cb8cd968b8d78a5c0
LuizRossini <luiz.guirossini123@gmail.com>
2024-09-05 17:38:46 -0300

    solving exercise 1002

--- src/main/java/exercises/beecrowd/exercise1002/Main.java
+++ src/main/java/exercises/beecrowd/exercise1002/Main.java
@@ -1,4 +1,19 @@
 package exercises.beecrowd.exercise1002;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double raio = scanner.nextDouble();
+        double n = Math.PI;
+        double area = n * (Math.pow(raio, 2));
+
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.US);
+        DecimalFormat df = new DecimalFormat("0.0000", symbols);
+
+        System.out.println("A=" +df.format(area));
+    }
 }
