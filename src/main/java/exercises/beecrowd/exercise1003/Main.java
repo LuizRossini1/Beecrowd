e0cd2c9aed3c98052e0a871faa2c4582fcc94485
LuizRossini <luiz.guirossini123@gmail.com>
2024-09-09 20:02:04 -0300

    exercise 1003 solved

--- src/main/java/exercises/beecrowd/exercise1003/Main.java
+++ src/main/java/exercises/beecrowd/exercise1003/Main.java
@@ -1,7 +1,13 @@
 package exercises.beecrowd.exercise1003;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int soma = a + b;
+        System.out.println("SOMA = " +soma);
     }
 }
